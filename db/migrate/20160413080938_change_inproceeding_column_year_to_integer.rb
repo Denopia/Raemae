10d7b126beec7336204228f85a037bc875c28e4d
Tuomo Torppa <totutotu@cs.helsinki.fi>
2016-04-13 11:48:32 +0300

    inproceedings ja articles sekä kaikki listaava sivu

--- db/migrate/20160413080938_change_inproceeding_column_year_to_integer.rb
+++ db/migrate/20160413080938_change_inproceeding_column_year_to_integer.rb
@@ -1,5 +1,5 @@
 class ChangeInproceedingColumnYearToInteger < ActiveRecord::Migration
   def change
-    change_column :inproceedings, :year, 'integer USING CAST(column_name AS integer)'
+    change_column :inproceedings, :year, 'integer USING CAST(year AS integer)'
   end
 end
